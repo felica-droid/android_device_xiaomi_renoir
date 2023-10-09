919fa89480d3e29123a6e43fad6b069d0458096c
Arian <arian.kulmer@web.de>
2023-10-09 20:02:18 +0200

    renoir: Enable camera override format from reserved
    This enables qcom additions for high framerate videos.
    
    Change-Id: I572d35c6454711e838b2d0dba31551627d10f6e6
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := renoir
 
+# Camera
+TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
+
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/hidl/manifest.xml
 
